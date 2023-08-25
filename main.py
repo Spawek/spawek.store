4553fa74f9db3aff9a182de2db251405256adb23
Maciej Oziębły <maciejoziebly@gmail.com>
2023-08-25 20:32:56 +0200

    Initial commit.

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_from_directory, request, g
+import random
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = 'mysecretkey_32meokfwerewlkewqjoi'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# TODO: log events
+# TODO: sitemap
